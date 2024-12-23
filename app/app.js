cceb2e28eef4edc41dfc7f2f2dea4ce57c0f361f
YOUNG ALA <ala.rah@outlook.com>
2024-12-23 22:18:13 +0000

    pod algorithm, some changes to db

--- app/app.js
+++ app/app.js
@@ -3,66 +3,88 @@ const express = require("express");
 const path = require("path");
 
 // Create express app
-var app = express();
+const app = express();
 
 // Add static files location
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "./views/pages"));
 
-app.use(express.static('public'));
-console.log("Serving static files from:", path.join(__dirname, 'public'));
-// app.set("views", "../static");
+app.use(express.static("public"));
+console.log("Serving static files from:", path.join(__dirname, "public"));
 
-console.log(app.get("views"), "----------------loggg");
 // Get the functions in the db.js file to use
 const db = require("./services/db");
 
-// app.get("/", function (req, res) {
-//   // sql = "select * from post";
-//   sql =
-//     "SELECT Post.post_id,Post.title,Post.image_url,Post.description,Post.location,Post.created_at AS post_created_at,User.user_id, User.username, User.email, User.profile_pic_url, User.gender, User.first_name, User.last_name, User.dob, User.age,User.created_at AS user_created_at FROM Post JOIN User ON Post.user_id = User.user_id";
-//   db.query(sql).then((results) => {
-//     console.log(results);
-//     res.render("home", { posts: results });
-//   });
-//   // res.render("home", { posts });
-// });
+// function to get Photo of the Day
+async function getPhotoOfTheDay() {
+  const now = new Date();
+  const last24Hours = new Date(now.getTime() - (24 * 60 * 60 * 1000)); // Get the timestamp for 24 hours ago
 
+  // Convert both dates to ISO strings for easier comparison
+  const nowISOString = now.toISOString();
+  const last24HoursISOString = last24Hours.toISOString();
+
+  const photoQuery = `
+    SELECT Post.image_url, Post.title, Post.description, COUNT(User_Likes_Post.post_id) AS like_count
+    FROM Post
+    JOIN User_Likes_Post ON Post.post_id = User_Likes_Post.post_id
+    WHERE User_Likes_Post.liked_at >= ?
+    GROUP BY Post.post_id
+    ORDER BY like_count DESC
+    LIMIT 1
+  `;
+
+  const photoResults = await db.query(photoQuery, [last24HoursISOString]);
+  return photoResults.length > 0 ? photoResults[0] : null;
+}
+// Home Route
 app.get("/", async function (req, res) {
-  // sql = "select * from post";
-  postsQuery =
-    "SELECT Post.post_id,Post.title,Post.image_url,Post.description,Post.location,Post.created_at AS post_created_at,User.user_id, User.username, User.email, User.profile_pic_url, User.gender, User.first_name, User.last_name, User.dob, User.age,User.created_at AS user_created_at FROM Post JOIN User ON Post.user_id = User.user_id";
-  photoOfTheDayQuery = 'SELECT image_url, title, description FROM Photo_of_the_day LIMIT 1';  
-  
-  const posts = await db.query(postsQuery);
-  const photoOfTheDayResults = await db.query(photoOfTheDayQuery);
-  
-    //console.log(results);
-    
-  
-  photoOfTheDay = photoOfTheDayResults.length > 0 ? photoOfTheDayResults[0] : null;
-  res.render("home", { posts: posts, photoOfTheDay: photoOfTheDay });
-  
-  
-  // res.render("home", { posts });
+  try {
+    // Fetch all posts
+    const postsQuery = `
+      SELECT Post.post_id, Post.title, Post.image_url, Post.description, Post.location, Post.created_at AS post_created_at,
+             User.user_id, User.username, User.email, User.profile_pic_url, User.gender, User.first_name, User.last_name,
+             User.dob, User.age, User.created_at AS user_created_at
+      FROM Post
+      JOIN User ON Post.user_id = User.user_id
+    `;
+    const posts = await db.query(postsQuery);
+
+    // Get Photo of the Day
+    const photoOfTheDay = await getPhotoOfTheDay();
+
+    // Render the home page with posts and Photo of the Day
+    res.render("home", { posts, photoOfTheDay });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
+// About Route
 app.get("/about", function (req, res) {
   res.render("about");
 });
 
+// Login Route
 app.get("/login", function (req, res) {
   res.render("login");
 });
 
-app.get("/post", function (req, res) {
-  sql = "select * from post";
-  db.query(sql).then((results) => {
-    console.log(results);
-  });
-  // res.render("posts", { posts });
+// Post Route
+app.get("/post", async function (req, res) {
+  try {
+    const postsQuery = "SELECT * FROM Post";
+    const posts = await db.query(postsQuery);
+    console.log(posts);
+    res.render("posts", { posts });
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
+// Signup Route
 app.get("/signup", function (req, res) {
   res.render("signup");
 });

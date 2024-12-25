7b95fcd84954146ae92ebe625c329bd017ae2c07
ShahriarGIT <antarx11@gmail.com>
2024-12-25 17:08:58 +0000

    updated post page

--- app/app.js
+++ app/app.js
@@ -1,17 +1,42 @@
 // Import express.js
 const express = require("express");
 const path = require("path");
+const multer = require("multer");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const dotenv = require("dotenv");
+const crypto = require("crypto");
 
+dotenv.config();
 // Create express app
 var app = express();
 
 // Add static files location
 app.use(express.static("public"));
 
+// Multer settings
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const accessKey = process.env.BUCKET_ACCESS_KEY;
+const secretAccessKey = process.env.BUCKET_SECRET_KEY;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey,
+  },
+  region: bucketRegion,
+});
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "./views/pages"));
 
 // app.set("views", "../static");
+
+const randomImageName = (bytes = 32) =>
+  crypto.randomBytes(bytes).toString("hex");
 
 console.log(app.get("views"), "----------------loggg");
 // Get the functions in the db.js file to use
@@ -22,7 +47,7 @@ app.get("/", function (req, res) {
   sql =
     "SELECT Post.post_id,Post.title,Post.image_url,Post.description,Post.location,Post.created_at AS post_created_at,User.user_id, User.username, User.email, User.profile_pic_url, User.gender, User.first_name, User.last_name, User.dob, User.age,User.created_at AS user_created_at FROM Post JOIN User ON Post.user_id = User.user_id";
   db.query(sql).then((results) => {
-    console.log(results);
+    // console.log(results);
     res.render("home", { posts: results });
   });
   // res.render("home", { posts });
@@ -34,6 +59,25 @@ app.get("/about", function (req, res) {
 
 app.get("/login", function (req, res) {
   res.render("login");
+});
+
+app.post("/create-post", upload.single("image"), async function (req, res) {
+  console.log(req.body);
+  console.log(req.file);
+  console.log(req.file.buffer);
+
+  const params = {
+    Bucket: bucketName,
+    Key: randomImageName(),
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype,
+  };
+
+  const command = new PutObjectCommand(params);
+
+  await s3.send(command);
+
+  // res.render("posts", { posts });
 });
 
 app.get("/post", function (req, res) {
@@ -48,34 +92,39 @@ app.get("/signup", function (req, res) {
   res.render("signup");
 });
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-const upload = multer({ storage });
-
-// Routes
-app.get("/upload", (req, res) => {
-  res.render("upload");
+app.get("/dashboard", function (req, res) {
+  res.render("upload-image");
 });
 
-app.post("/upload", upload.single("image"), (req, res) => {
-  const { title, description, location } = req.body;
-  const imagePath = `/uploads/${req.file.filename}`;
+// ------------------ upload image --------------------
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+// const upload = multer({ storage });
 
-  console.log("Uploaded Image Details:");
-  console.log({ title, description, location, imagePath });
+// // Routes
+// app.get("/upload", (req, res) => {
+//   res.render("upload");
+// });
 
-  res.send(`<h1>Image Uploaded Successfully!</h1>
-            <p>Title: ${title}</p>
-            <p>Description: ${description}</p>
-            <p>Location: ${location}</p>
-            <img src="${imagePath}" alt="${title}" style="max-width: 100%;">`);
-});
+// app.post("/upload", upload.single("image"), (req, res) => {
+//   const { title, description, location } = req.body;
+//   const imagePath = `/uploads/${req.file.filename}`;
+
+//   console.log("Uploaded Image Details:");
+//   console.log({ title, description, location, imagePath });
+
+//   res.send(`<h1>Image Uploaded Successfully!</h1>
+//             <p>Title: ${title}</p>
+//             <p>Description: ${description}</p>
+//             <p>Location: ${location}</p>
+//             <img src="${imagePath}" alt="${title}" style="max-width: 100%;">`);
+// });
 
 // Start server on port 3000
 app.listen(3000, function () {
---
+++ public/js/script-dashboard.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const dashboard_menu = document.querySelectorAll(".menu-button");
+
+dashboard_menu.forEach((item) => {
+  item.addEventListener("click", function () {
+    console.log(this);
+    console.log(this.innerText);
+  });
+});
+
+// console.log(dashboard_menu);
